af2060aadd67e2eb7fa0cde3be1a52d94979f6b7
Sel <selcuk.80@outlook.de>
2025-06-21 13:47:36 +0200

    test(ci): use Puppeteer Chrome binary

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,10 @@
  // karma.conf.js
-module.exports = function (config) {
+
+const puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
+
+
+ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
