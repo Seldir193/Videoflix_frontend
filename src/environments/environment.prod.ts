abb524ff8cd30c177f67c1c58ef051ee838d80d6
Sel <selcuk.80@outlook.de>
2025-06-26 10:18:13 +0200

    deploy test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,10 +13,10 @@ export const environment = {
   //apiUrl  : 'https://videoflix.selcuk-kocyigit.de/api',
   //authUrl : 'https://videoflix.selcuk-kocyigit.de/api/auth',
 
-  apiUrl:  'https://videoflix-api.herokuapp.com/api',
-  authUrl: 'https://videoflix-api.herokuapp.com/api/auth',
+  //apiUrl:  'https://videoflix-api.herokuapp.com/api',
+  //authUrl: 'https://videoflix-api.herokuapp.com/api/auth',
 
-  //apiUrl:  'https://api.videoflix.selcuk-kocyigit.de/api',
- // authUrl: 'https://api.videoflix.selcuk-kocyigit.de/api/auth',
+  apiUrl:  'https://api.videoflix.selcuk-kocyigit.de/api',
+  authUrl: 'https://api.videoflix.selcuk-kocyigit.de/api/auth',
   staticUrl: 'https://videoflix.selcuk-kocyigit.de/',
 };
